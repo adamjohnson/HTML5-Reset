68a45228cbf566a9118c15394c246661f998f423
adamjohnson <riotaj@hotmail.com>
2012-09-27 00:49:57 -0400

    Updating footer info to be congruent with Greek Yogurt
    More modern footer configuration.

--- theme_htmlfive_reset/themes/htmlfive_reset/elements/footer.php
+++ theme_htmlfive_reset/themes/htmlfive_reset/elements/footer.php
@@ -6,23 +6,27 @@
             $a->display($c); // footer editable region
         ?> 
     
-        <span class="powered-by"><a href="http://www.concrete5.org" title="<?php    echo t('concrete5 open source CMS')?>"><?php    echo t('concrete5 Content Management')?></a> &amp; <a href="http://html5reset.org"><?php    echo t('HTML5 Reset')?></a></span>
-                    &copy; <?php    echo date('Y')?> <a href="<?php    echo DIR_REL?>/"><?php       echo SITE?></a>.
-                    <?php    echo t('All rights reserved.')?>
-                    <?php       
-                    $u = new User();
-                    if ($u->isRegistered()) { ?>
-                        <?php        
-                        if (Config::get("ENABLE_USER_PROFILES")) {
-                            $userName = '<a href="' . $this->url('/profile') . '">' . $u->getUserName() . '</a>';
-                        } else {
-                            $userName = $u->getUserName();
-                        }
-                        ?>
-                        <span class="sign-in"><?php    echo t('Currently logged in as <strong>%s</strong>.', $userName)?> <a href="<?php    echo $this->url('/login', 'logout')?>"><?php    echo t('Sign Out')?></a></span>
-                    <?php    } else { ?>
-                        <span class="sign-in"><a href="<?php    echo $this->url('/login')?>"><?php    echo t('Sign In to Edit this Site')?></a></span>
-                    <?php    } ?>
+            <p class="footer-sign-in">
+            <?php  
+            $u = new User();
+            if ($u->isRegistered()) { ?>
+                <?php   
+                if (Config::get("ENABLE_USER_PROFILES")) {
+                    $userName = '<a href="' . $this->url('/profile') . '">' . $u->getUserName() . '</a>';
+                } else {
+                    $userName = $u->getUserName();
+                }
+                ?>
+                <span class="sign-in"><?php  echo t('Currently logged in as <b>%s</b>.', $userName)?> <a href="<?php  echo $this->url('/login', 'logout')?>"><?php  echo t('Sign Out')?></a></span>
+            <?php   } else { ?>
+                <span class="sign-in"><a href="<?php  echo $this->url('/login')?>"><?php  echo t('Sign In to Edit this Site')?></a></span>
+            <?php   } ?>
+            </p>
+            
+            <div class="clear"></div>
+            <p class="footer-copyright">&copy;<?php  echo date('Y')?> <?php  echo SITE?>.</p>
+            <p class="footer-tag-line"><?php echo t('Built with <a href="http://www.concrete5.org/" alt="Free Content Management System" target="_blank">concrete5 - an open source CMS')?></a></p>
+    
     </footer> <!-- close footer -->
 </div> <!-- close .wrapper -->
 
